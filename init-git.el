4516fc95a6c413b35fd60ab1fc56507e45188230
Jarkko Oranen <jarkko.oranen@tut.fi>
2009-08-20 22:40:25 +0300

    Trying out magit

--- init-git.el
+++ init-git.el
@@ -2,6 +2,6 @@
 (when (featurep 'vc-git) (add-to-list 'vc-handled-backends 'git))
 (autoload 'git-blame-mode "git-blame" "Minor mode for incremental blame for Git." t)
 (require 'git) 
-(require 'egg)
+(require 'magit)
 
 (provide 'init-git)
