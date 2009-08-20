1a64d8a75c865fa6d739aee752be5fd096a38eb7
Jarkko Oranen <jarkko.oranen@tut.fi>
2009-08-20 21:08:15 +0300

    Clean up Clojure SLIME setup

--- init-clojure.el
+++ init-clojure.el
@@ -1,7 +1,22 @@
 (require 'swank-clojure-autoload)
 
+(setq swank-clojure-jar-path "~/opt/lisp/clojure/clojure.jar")
+(unless (boundp 'swank-clojure-extra-classpaths)
+  (setq swank-clojure-extra-classpaths nil))
+(add-to-list 'swank-clojure-extra-classpaths
+             "/Users/oranenj/opt/lisp/clojure/clojure-contrib/src/")
+(add-to-list 'swank-clojure-extra-classpaths
+             "/Users/oranenj/opt/lisp/clojure/src/clj/")
+
 (eval-after-load 'clojure-mode
-  '(clojure-slime-config "~/opt/lisp/"))
+  '(progn
+     (require 'slime)
+     (setf slime-lisp-implementations
+           `((clojure ,(swank-clojure-cmd)
+                      :init swank-clojure-init
+                      :coding-system utf-8-unix)))
+     (setq slime-net-coding-system 'utf-8-unix)
+     (slime-setup '(slime-fancy))))
 
 (provide 'init-clojure)
 
